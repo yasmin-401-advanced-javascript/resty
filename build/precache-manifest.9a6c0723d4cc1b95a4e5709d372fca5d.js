6257b2dc8e1f15cdb051d1d317c67bf953e83801
yasminadaileh1 <60566993+yasminadaileh1@users.noreply.github.com>
2020-06-29 21:31:22 +0300

    change orgaizing the file

---
+++ build/precache-manifest.9a6c0723d4cc1b95a4e5709d372fca5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72bb4127c45a36fe91f8bada8b752aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48fa039e5816bf44aa3",
+    "url": "/static/css/main.cdcf4ad2.chunk.css"
+  },
+  {
+    "revision": "f1b9fe14f2afe522a17b",
+    "url": "/static/js/2.2c2e7c3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c2e7c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48fa039e5816bf44aa3",
+    "url": "/static/js/main.239d8b86.chunk.js"
+  },
+  {
+    "revision": "411f183aa86445d317cd",
+    "url": "/static/js/runtime-main.8eb541cb.js"
+  }
+]);
